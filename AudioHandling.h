ef699fb50cff94fd738eb086a5234bbdd1fe3050
skyywaffle <1celiminal@gmail.com>
2025-04-23 02:58:18 -0400

    Reverted chatgpt ahh coding

--- AudioHandling.h
+++ AudioHandling.h
@@ -3,10 +3,4 @@
 #include "Input.h"
 #include <libsndfile/sndfile.h>
 
-void createWavFromSilence(const std::string &effectWav,
-                          const std::string &outputWav,
-                          std::vector<int> insertPoints,
-                          int totalDuration,
-                          int sampleRate);
-
 bool generateAudio(const Macro &macro);
