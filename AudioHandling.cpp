16cb5a9eb6b24dfd024d240a8d2fee3783047b57
skyywaffle <1celiminal@gmail.com>
2025-04-26 21:03:05 -0400

    Refactored generateAudio function

--- AudioHandling.cpp
+++ AudioHandling.cpp
@@ -12,6 +12,13 @@
 
 namespace fs = std::filesystem;
 
+struct AudioFile
+{
+    SF_INFO info;
+    SNDFILE *file;
+    std::vector<short> buffer;
+};
+
 void mix_click(std::vector<short> &outBuffer, const std::vector<short> &click, int insertIndex, int channels)
 {
     for (size_t i = 0; i < click.size(); ++i)
@@ -30,116 +37,67 @@ void mix_click(std::vector<short> &outBuffer, const std::vector<short> &click, i
     }
 }
 
-struct AudioFile
+std::vector<AudioFile> getAudioFiles(const char* folderName)
 {
-    SF_INFO info;
-    SNDFILE *file;
-    std::vector<short> buffer;
-};
+    std::vector<AudioFile> files{};
+
+    fs::path folderPath{folderName};
+    if (fs::exists(folderPath) && fs::is_directory(folderPath))
+    {
+        for (const auto &item : fs::directory_iterator(folderPath))
+        {
+            // Construct the filename and add a new AudioFile to the files vector
+            if (fs::is_regular_file(item))
+            {
+                std::string fileString{folderName};
+                fileString.append("/");
+                fileString.append(item.path().filename().string());
+                AudioFile audioFile{};
+                audioFile.file = sf_open(fileString.c_str(), SFM_READ, &audioFile.info);
+
+                audioFile.buffer.resize(audioFile.info.frames * audioFile.info.channels);
+                sf_read_short(audioFile.file, audioFile.buffer.data(), audioFile.buffer.size());
+                sf_close(audioFile.file);
+
+                files.push_back(audioFile);
+            }
+        }
+    }
+    else // if the folder name is invalid
+    {
+        std::cerr << "Invalid folder name: " << folderName << '\n';
+    }
+    return files;
+}
+
+void addToBuffer(std::vector<float>& inputTimes, std::vector<AudioFile>& files, std::vector<short>& buffer, int sampleRate, int channels)
+{
+    // Add clicks to output buffer
+    for (float t : inputTimes)
+    {
+        // set up random index choice
+        std::random_device rd;
+        std::mt19937 gen(rd());
+        std::uniform_int_distribution<> distrib(0, files.size() - 1);
+
+        // Add to buffer
+        sf_count_t frameIndex = static_cast<sf_count_t>(t * sampleRate);
+        int sampleIndex = frameIndex * channels; // Interleaved
+        mix_click(buffer, files[distrib(gen)].buffer, sampleIndex, channels);
+    }
+}
 
 // Returns whether audio generation succeeded
 bool generateAudio(const Macro &macro)
 {
-    std::vector<AudioFile> clicks{};
-    std::vector<AudioFile> releases{};
-    std::vector<AudioFile> softClicks{};
-    std::vector<AudioFile> softReleases{};
-
-    // iterate over files in "clicks" folder, add each to clicks vector
-    fs::path clicksPath{"clicks"};
-    if (fs::exists(clicksPath) && fs::is_directory(clicksPath))
-    {
-        for (const auto &item : fs::directory_iterator(clicksPath))
-        {
-            // Construct the filename and add a new AudioFile to clicks
-            if (fs::is_regular_file(item))
-            {
-                std::string fileString{clicksPath.string()};
-                fileString.append("/");
-                fileString.append(item.path().filename().string());
-                AudioFile af{};
-                af.file = sf_open(fileString.c_str(), SFM_READ, &af.info);
-
-                af.buffer.resize(af.info.frames * af.info.channels);
-                sf_read_short(af.file, af.buffer.data(), af.buffer.size());
-                sf_close(af.file);
-
-                clicks.push_back(af);
-            }
-        }
-    }
-
-    // iterate over files in "releases" folder, add each to releases vector
-    fs::path releasesPath{"releases"};
-    if (fs::exists(releasesPath) && fs::is_directory(releasesPath))
-    {
-        for (const auto &item : fs::directory_iterator(releasesPath))
-        {
-            if (fs::is_regular_file(item))
-            {
-                std::string fileString{releasesPath.string()};
-                fileString.append("/");
-                fileString.append(item.path().filename().string());
-                AudioFile af{};
-                af.file = sf_open(fileString.c_str(), SFM_READ, &af.info);
-
-                af.buffer.resize(af.info.frames * af.info.channels);
-                sf_read_short(af.file, af.buffer.data(), af.buffer.size());
-                sf_close(af.file);
-
-                releases.push_back(af);
-            }
-        }
-    }
-
-    // iterate over files in "softclicks" folder, add each to softClicks vector
-    fs::path softClicksPath{"softclicks"};
-    if (fs::exists(softClicksPath) && fs::is_directory(softClicksPath))
-    {
-        for (const auto &item : fs::directory_iterator(softClicksPath))
-        {
-            if (fs::is_regular_file(item))
-            {
-                std::string fileString{softClicksPath.string()};
-                fileString.append("/");
-                fileString.append(item.path().filename().string());
-                AudioFile af{};
-                af.file = sf_open(fileString.c_str(), SFM_READ, &af.info);
-
-                af.buffer.resize(af.info.frames * af.info.channels);
-                sf_read_short(af.file, af.buffer.data(), af.buffer.size());
-                sf_close(af.file);
-
-                softClicks.push_back(af);
-            }
-        }
-    }
-
-    // iterate over files in "softreleases" folder, add each to softReleases vector
-    fs::path softReleasesPath{"softreleases"};
-    if (fs::exists(softReleasesPath) && fs::is_directory(softReleasesPath))
-    {
-        for (const auto &item : fs::directory_iterator(softReleasesPath))
-        {
-            if (fs::is_regular_file(item))
-            {
-                std::string fileString{softReleasesPath.string()};
-                fileString.append("/");
-                fileString.append(item.path().filename().string());
-                AudioFile af{};
-                af.file = sf_open(fileString.c_str(), SFM_READ, &af.info);
-
-                af.buffer.resize(af.info.frames * af.info.channels);
-                sf_read_short(af.file, af.buffer.data(), af.buffer.size());
-                sf_close(af.file);
-
-                softReleases.push_back(af);
-            }
-        }
-    }
+    // Get click audio files
+    std::vector<AudioFile> clicks{getAudioFiles("clicks")};
+    std::vector<AudioFile> releases{getAudioFiles("releases")};
+    std::vector<AudioFile> softClicks{getAudioFiles("softclicks")};
+    std::vector<AudioFile> softReleases{getAudioFiles("softreleases")};
 
     // Define the total duration in seconds of the output file
-    float durationSeconds = (float)(macro.getDurationInSec() + 1);
+    float durationSeconds = (float)(macro.getDurationInSec() + 1); // add an extra second so no releases get cut off
     int sampleRate = clicks[0].info.samplerate;
     int channels = clicks[0].info.channels;
     sf_count_t totalFrames = static_cast<sf_count_t>(durationSeconds * sampleRate);
@@ -151,6 +109,7 @@ bool generateAudio(const Macro &macro)
     std::vector<float> softClickTimes{};
     std::vector<float> softReleaseTimes{};
 
+    // Add the times of inputs to their corresponding vectors
     for (Input i : macro.getInputs())
     {
         if (i.isDown())
@@ -177,54 +136,11 @@ bool generateAudio(const Macro &macro)
         }
     }
 
-    // Add clicks to output buffer
-    for (float t : clickTimes)
-    {
-        // set up random click picker
-        std::random_device rd;
-        std::mt19937 gen(rd());
-        std::uniform_int_distribution<> distrib(0, clicks.size() - 1);
-
-        sf_count_t frameIndex = static_cast<sf_count_t>(t * sampleRate);
-        int sampleIndex = frameIndex * channels; // Interleaved
-        mix_click(outputBuffer, clicks[distrib(gen)].buffer, sampleIndex, channels);
-    }
-
-    // Add releases to output buffer
-    for (float t : releaseTimes)
-    {
-        std::random_device rd;
-        std::mt19937 gen(rd());
-        std::uniform_int_distribution<> distrib(0, releases.size() - 1);
-
-        sf_count_t frameIndex = static_cast<sf_count_t>(t * sampleRate);
-        int sampleIndex = frameIndex * channels;
-        mix_click(outputBuffer, releases[distrib(gen)].buffer, sampleIndex, channels);
-    }
-
-    // Add soft clicks to output buffer
-    for (float t : softClickTimes)
-    {
-        std::random_device rd;
-        std::mt19937 gen(rd());
-        std::uniform_int_distribution<> distrib(0, softClicks.size() - 1);
-
-        sf_count_t frameIndex = static_cast<sf_count_t>(t * sampleRate);
-        int sampleIndex = frameIndex * channels;
-        mix_click(outputBuffer, softClicks[distrib(gen)].buffer, sampleIndex, channels);
-    }
-
-    // Add soft releases to output buffer
-    for (float t : softReleaseTimes)
-    {
-        std::random_device rd;
-        std::mt19937 gen(rd());
-        std::uniform_int_distribution<> distrib(0, softReleases.size() - 1);
-
-        sf_count_t frameIndex = static_cast<sf_count_t>(t * sampleRate);
-        int sampleIndex = frameIndex * channels;
-        mix_click(outputBuffer, softReleases[distrib(gen)].buffer, sampleIndex, channels);
-    }
+    // Add click sounds to the output buffer
+    addToBuffer(clickTimes, clicks, outputBuffer, sampleRate, channels);
+    addToBuffer(releaseTimes, releases, outputBuffer, sampleRate, channels);
+    addToBuffer(softClickTimes, softClicks, outputBuffer, sampleRate, channels);
+    addToBuffer(softReleaseTimes, softReleases, outputBuffer, sampleRate, channels);
 
     // Write to new WAV file
     SF_INFO sfinfoOut = clicks[0].info;
