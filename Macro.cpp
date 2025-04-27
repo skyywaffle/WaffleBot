6092edfbcf9c7eb901f9df0a9158cb0f1c5ce28f
skyywaffle <1celiminal@gmail.com>
2025-04-27 17:45:52 -0400

    Last fix for real

--- Macro.cpp
+++ Macro.cpp
@@ -18,15 +18,9 @@ Macro::Macro(std::string filepath)
         std::cerr << "No config JSON found!\n";
         std::exit(1);
     }
-    if (!fs::exists(filepath))
-    {
-        std::cerr << filepath << " not found!\n";
-        std::exit(1);
-    }
     Json clickConfig = Json::parse(std::ifstream("config.json"));
 
-    
-    m_name = fs::path(filepath).filename().string();
+        m_name = fs::path(filepath).filename().string();
     m_jsonData = Json::parse(std::ifstream{filepath});
     m_framerate = m_jsonData["framerate"];
     m_durationInSec = m_jsonData["duration"];
